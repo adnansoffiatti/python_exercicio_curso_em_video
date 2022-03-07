67bf455a68ac39011396d4d444c98cf27d3608e2
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-07 06:43:50 -0300

    Mundo 1 - exercicio 5

---
+++ mundo1/3_calculos_e_operacoes/exercicio5_antecessor_e_sucessor.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor
+# e seu antecessor:
+
+n = int(input('Digite um númeiro: '))
+print(f'O antecessor do número {n} é {n - 1}')
+print(f'O sucessor do número {n} é {n + 1}')

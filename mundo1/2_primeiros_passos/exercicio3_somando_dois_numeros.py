641e62fccb6707467971bf972320e47740d5929a
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-06 21:25:22 -0300

    Reeorganização de pastas e exercicio 3

---
+++ mundo1/2_primeiros_passos/exercicio3_somando_dois_numeros.py
@@ -0,0 +1,7 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+print('A soma é:', num1 + num2)
+print('A soma é: %d' % (num1 + num2))
+print(f'A soma é: {num1 + num2}')

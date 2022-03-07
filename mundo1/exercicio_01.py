e97b5db96ba16b65d1be2fa5c0e6475ebc4788c2
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-06 21:06:08 -0300

    Mundo 1 - exercicio1

---
+++ mundo1/exercicio_01.py
@@ -0,0 +1,13 @@
+'''
+Crie um script Python que leia o nome de uma pessoa e mostre uma
+mensagem de boas-vindas de acordo com o valor digitado
+'''
+
+entrada = input('Digite seu nome: \n')
+print(f'Olá, {entrada}! Prazer em conhecer você!')
+
+# Crie um programa que escreva "Olá mundo" na tela.
+
+print('Olá mundo!')
+msg = 'Hello world!'
+print(msg)

585fce7e1d91fa8acd0e147ba7493b5c26624746
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-06 21:33:32 -0300

    Mundo 1 - exercicio 4

---
+++ mundo1/2_primeiros_passos/exercicio4_dissecando_uma_variavel.py
@@ -0,0 +1,13 @@
+# Faça um programa que leia algo pelo teclado
+# e mostre na tela o seu tipo primitivo
+# e todas as informações possíveis sobre ele
+
+entrada = input('Digite algo: ')
+print(f'Tipo primitivo: {entrada}')
+
+print(f'É númerico? {entrada.isnumeric}')
+print(f'É alfanúmerico? {entrada.isalpha}')
+print(f'É um decimal? {entrada.isdecimal}')
+print(f'Está em caixa baixa? {entrada.islower}')
+print(f'É apenas espaço em branco? {entrada.isspace}')
+print(f'Esta em caixa alta? {entrada.isupper}')

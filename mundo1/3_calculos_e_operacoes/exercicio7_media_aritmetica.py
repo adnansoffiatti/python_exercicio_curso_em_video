178a8ea47adb4b67fb8cb67759ad35eac8414682
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-07 06:53:22 -0300

    Mundo 1 - exercicio 7

---
+++ mundo1/3_calculos_e_operacoes/exercicio7_media_aritmetica.py
@@ -0,0 +1,9 @@
+'''
+Desenvolva um programa que leia as duas notas de um aluno,
+calcule e mostre a sua média
+'''
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+print(f'A média das notas {nota1} e {nota2} é {media}!')

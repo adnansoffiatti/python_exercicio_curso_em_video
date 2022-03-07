bd2468c9eb1614821006248a811f19d59d67626d
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-07 06:48:01 -0300

    Mundo 1 - exercicio 6

---
+++ mundo1/3_calculos_e_operacoes/exercicio6_dobro_triplo_raiz_quadrada.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, o seu
+# triplo e sua raiz quadrada
+
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} é {n * 2}')
+print(f'O triplo de {n} é {n * 3}')
+print(f'A raiz quadrada de {n} é {n**(1/2)}')

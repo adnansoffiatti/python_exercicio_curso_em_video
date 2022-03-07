9ddebef55ffc4901b98d55c360061ddec3165c1f
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-07 12:29:19 -0300

    Mundo 1 - exercicio 11

---
+++ mundo1/3_calculos_e_operacoes/exercicio11_pintando_parede.py
@@ -0,0 +1,13 @@
+'''
+Faça um programa que leia a largura e a altura de uma parede em metros,
+calcule a sua área e a quantidade de tinta necessária para pintá-la,
+sabendo que cada litro de tinta pinta uma área de 2m²
+'''
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+area = larg * alt
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m²'
+      .format(larg, alt, area))
+
+tinta = area / 2
+print('Para pintar essa parede, você precisará de {}l de tinta.'.format(tinta))

704bbee7846857d1acd7750c52ce786713b440f9
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-08 06:27:43 -0300

    Mundo 1 - exercicio 13

---
+++ mundo1/3_calculos_e_operacoes/exercicio13_reajuste_salarial.py
@@ -0,0 +1,6 @@
+# Faça um algoritmo que leia o salário de um funcionário e
+# mostre seu novo salário, com 15% de aumento
+
+salario = float(input('Qual é o salário do Funcionário? R$'))
+novo = salario + (salario * 15 / 100)
+print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(salario, novo))

d05d0cb3b958affbf72d44cd46bebd767bc5b1f7
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-08 09:23:45 -0300

    Mundo 1 - Modulo 4 - exercicio 21

---
+++ mundo1/4_modulos/exercicio21_tocando_um_mp3.py
@@ -0,0 +1,12 @@
+'''
+Faça um programa em Python que abra e reproduza o áudio
+de um arquivo MP3
+'''
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('solo.mp3')
+pygame.micer.music.play()
+pygame.event.wait()
+
+# Não rodou :,(

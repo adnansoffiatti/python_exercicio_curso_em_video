a7e19abe9f6b5f0cf1bf2c70d8f038555a25a669
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-08 06:38:59 -0300

    Mundo 1 - exercicio 14

---
+++ mundo1/3_calculos_e_operacoes/exercicios14_conversor_de_temperaturas.py
@@ -0,0 +1,8 @@
+'''
+Escreva um programa que converta uma temperatura digitada em °C
+e converta para °F.
+'''
+
+c = float(input('Informe a temperatura em °C: '))
+f = ((9 * c) / 5) + 32
+print('A temperatura de {}°C corresponde a {}°F'.format(c, f))

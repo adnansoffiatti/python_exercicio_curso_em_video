bb478ff0f25806d689a947d7d0bc5b03bcb43277
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-09 05:48:33 -0300

    Mundo 1 - String - exercicio 25

---
+++ mundo1/5_strings/exercicio25_procurando_string_dentro_de_outra.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que leia o nome de uma pessoa e diga se ela tem
+"SILVA" no nome.
+'''
+
+nome = str(input('Qual é seu nome completo? ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))

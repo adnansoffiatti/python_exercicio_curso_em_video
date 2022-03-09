492d7fdb143d567fc5612acb343e886490c62f4f
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-09 06:17:09 -0300

    Mundo 1 - String - exercicio 27

---
+++ mundo1/5_strings/exercicio27_primeiro_e_ultimo_nome_de_uma_pessoa.py
@@ -0,0 +1,13 @@
+'''
+Faça um programa que leia o nome completo de uma pessoa, mostrando em
+seguida o primeiro e o último nome separadamente.
+Ex: Ana Maria de Souza
+primeiro = Ana
+último = Souza
+'''
+
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))

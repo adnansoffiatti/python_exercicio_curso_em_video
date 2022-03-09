b001ff1fbd224922056774829d1e483823d2c5c2
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-09 09:54:34 -0300

    Mundo 1 - Estrutura Condicional - exercicio 35

---
+++ mundo1/6_estruturas_condicionais/exercicio35_analisando_triangulo.py
@@ -0,0 +1,16 @@
+'''
+Desenvolva um programa que leia o comprimento
+de três restas e diga ao usuário se elas podem ou
+não formar um triângulo
+'''
+
+print('-=' * 20)
+print('Analisador de Trinângulos')
+print('-=' * 20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segumento: '))
+r3 = float(input('Terceito segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo!')

27d54ef026bba3e4487b784488c1c56256f54e1a
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-10 06:59:43 -0300

    Mundo 2 - Estruturas de controle - exercicio 36

---
+++ mundo2/1_condicoes_em_python/exercicio36_aprovando_emprestimo.py
@@ -0,0 +1,20 @@
+'''
+Escreva um programa para aprovar o emprestimo bancario para a
+compra de uma casa. Pergunte o valor da casa, o salario do comprador
+e em quantos anos ele vai pagar.
+A prestação mensal, não pode exceder 30% do salário ou então o
+emprestimo sera negado
+'''
+
+casa = float(input('Valor da casa: R$ '))
+salario = float(input('Salario do comprador: R$ '))
+anos = int(input('Quantos anos de financiamento? '))
+prestacao = casa / (anos * 12)
+minimo = salario * 30 / 100
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end='')
+print(' a prestação será de R${:.2f}'.format(prestacao))
+
+if prestacao <= minimo:
+    print('Emprestimo pode ser CONCEDIDO!')
+else:
+    print('Emprestimo NEGADO!')

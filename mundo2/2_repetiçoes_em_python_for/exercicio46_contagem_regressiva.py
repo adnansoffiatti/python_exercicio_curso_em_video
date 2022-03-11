73ac42e8a9d1ccf710dfbb3957e64afa36204dd9
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-11 07:13:03 -0300

    Mundo 2 - Repetições em Python - exercicio 46

---
+++ mundo2/2_repetiçoes_em_python_for/exercicio46_contagem_regressiva.py
@@ -0,0 +1,12 @@
+'''
+Faça um programa que mostre na tela uma
+CONTAGEM REGRESSIVA para o estouro de
+fogos de artifício, indo de 10 até 0, com
+uma pausa de 1 segundo entre eles
+'''
+from time import sleep
+
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(0.5)
+print('BUM BUM! POW')

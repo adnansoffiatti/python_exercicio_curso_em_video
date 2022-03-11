61d0ef02d76222ee9d3c7431e58b3b012d354ca2
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-11 06:00:48 -0300

    Mundo 2 - Estruturas de Controle - exercicio 44

--- mundo2/1_condicoes_em_python/exercicio44_gerenciador_de_pagamentos.py
+++ mundo2/1_condicoes_em_python/exercicio44_gerenciador_de_pagamentos.py
@@ -19,5 +19,19 @@ if opcao == 1:
     total = preco - (preco * 10 / 100)
 elif opcao == 2:
     total = preco - (preco * 5 / 100)
+elif opcao == 3:
+    total = preco
+    parcela = total / 2
+    print(
+        'Sua compra será parcelada em 2x de R${:.2f} SEM JUROS'.format(parcela))
+elif opcao == 4:
+    total = preco + (preco * 20 / 100)
+    total_parecelas = int(input('Quantas parcelas? '))
+    parcela = total / total_parecelas
+    print('Sua compra será parcelada em {}x de R$ {:.2f} COM JUROS'.format(
+        total_parecelas, parcela))
+else:
+    total = 0
+    print('OPÇÃO INVÁLIDA de pagamento. Tente novamente')
 print('Sua compra de R$ {:.2f} vai custar R$ {:.2f} no final'.format(
     preco, total))

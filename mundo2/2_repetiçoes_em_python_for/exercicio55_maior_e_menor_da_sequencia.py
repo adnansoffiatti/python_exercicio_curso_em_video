a61ebcfa88ec7859db5fd42851777076a03e3731
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-11 11:32:58 -0300

    Mundo 2 - Repetições em Python For - exercicio 55

---
+++ mundo2/2_repetiçoes_em_python_for/exercicio55_maior_e_menor_da_sequencia.py
@@ -0,0 +1,19 @@
+'''
+Faça um prgorama que leia o PESO de
+CINCO PESSOAS; No final, mostre qual
+foi o MAIOR e o MENOR peso lidos.
+'''
+maior = 0
+menor = 0
+for p in range(1, 6):
+    peso = float(input('Peso da {}° pessoa: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}Kg'.format(maior))
+print('O menor peso lido foi de {}Kg'. format(menor))

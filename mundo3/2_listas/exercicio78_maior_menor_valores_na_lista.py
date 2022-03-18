d223812030f3d5fb46e33dd65178ad08436a02bc
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-18 09:34:32 -0300

    Mundo 3 - Listas - exercicio 78

---
+++ mundo3/2_listas/exercicio78_maior_menor_valores_na_lista.py
@@ -0,0 +1,31 @@
+'''
+Faça um programa que leia 5 VALORES NÚMERICOS e guarde-os em
+uma LISTA. No final, mostre qual foi o MAIOR e o MENOR valor
+digitado e as suas respectivas POSIÇÕES na lista.
+'''
+
+listanum = []
+maior = 0
+menor = 0
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a Posição {c}: ')))
+    if c == 0:
+        maior = menor = listanum[c]
+    else:
+        if listanum[c] > maior:
+            maior = listanum[c]
+        if listanum[c] < menor:
+            menor = listanum[c]
+
+print('=-' * 30)
+print(f'Você digitou os valores {listanum}')
+print(f'O maior valor digitado foi {maior} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == maior:
+        print(f'{i}... ', end='')
+print()
+print(f'O menor valor digitado foi {menor} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == menor:
+        print(f'{i}... ', end='')
+print()

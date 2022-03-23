cf9bc75e523887dc85e845c49d4bb36c3ec4b0dd
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-03-23 07:04:48 -0300

    Mundo 3 - Listas parte 2 - exercicio 84

---
+++ mundo3/3_listas_parte_2/exercicio84_lista_composta_analise_de_dados.py
@@ -0,0 +1,38 @@
+'''
+Faça um programa que leia NOME e PESO de VÁRIAS PESSOAS,
+guardando tudo em uma LISTA. No final mostre:
+a) QUANTAS pessoas foram cadastradas
+b) Uma listagem com as pessoas mais PESADAS
+c) Uma listagem com as pessoas mais LEVES.
+'''
+
+temp = []
+princ = []
+maior = menor = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'Ao todo, você cadastrou {len(princ)} pessoas. ')
+print(f'O maior peso foi de {maior}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == maior:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso foi de {menor}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == menor:
+        print(f'[{p[0]}] ', end='')
+print()
